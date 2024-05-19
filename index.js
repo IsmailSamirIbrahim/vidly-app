2b76b335a4eea5d5b79bdc79c7cb275b8308b99a
IsmailSamirIbrahim <ismail.samir.ibrahim@gmail.com>
2024-05-19 21:26:46 +0300

    cleanup code

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const mongoose = require('mongoose');
 const helmet   = require('helmet');
 const morgan   = require('morgan');
 const debug    = require('debug')('vidly::startup');
+const express  = require('express');
 
 mongoose.connect('mongodb://localhost/vidly')
 .then(() => debug("Connected to mongodb..."))
 .catch((err) => debug(err.message));
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extends: true}));
 app.use(helmet());
 
 if(app.get('env') === 'development') {
     app.use(morgan('tiny'));
     debug('morgan is enabled...');
 }
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { debug(`Listen to port ${port}`) });
