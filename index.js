df795173710ef3af3dbc3b7f0528657ef461c39b
IsmailSamirIbrahim <ismail.samir.ibrahim@gmail.com>
2024-05-19 21:11:58 +0300

    integrate mongodb to the vidly app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+mongoose.connect('mongodb://localhost/vidly')
+.then(() => console.log("Connected to mongodb..."))
+.catch((err) => console.log(err.message));
+
 
 const port = 3000;
 app.listen(port, () => {console.log(`Connected to the port number:${port}`)});
