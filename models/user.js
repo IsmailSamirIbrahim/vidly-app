4398fabeb8121f0b3c82e520654571016b556425
Ismail Samir Ibrahim <ismail.samir.ibrahim@gmail.com>
2024-05-22 20:41:21 +0300

    PR-9: Create an user endpoints to handle http requests (#10)
    * install jwt and bcrypt packages
    
    * create an user model
    
    * Create an user endpoints to handle http requests

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const Joi      = require('joi');
+const mongoose = require('mongoose');
+const jwt      = require('jsonwebtoken');
+const config   = require('config');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 15,
+        maxLength: 50,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 1024
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id}, config.get('jwtPrivateKey'));
+    return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        username: Joi.string().min(5).max(50).required(),
+        email:    Joi.string().min(5).max(50).required().email(),
+        password: Joi.string().min(5).max(50).required(),
+        isAdmin:  Joi.boolean().required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+module.exports.User     = User;
+module.exports.validate = validateUser;
