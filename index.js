834127bd22021d639e583cca68ea2d8a5271f9e3
IsmailSamirIbrahim <ismail.samir.ibrahim@gmail.com>
2024-05-23 20:17:36 +0300

    PR-17: Extracting the Config and validation logic

--- index.js
+++ index.js
@@ -1,62 +1,13 @@
-const Joi          = require('joi');
-Joi.objectId       = require('joi-objectid')(Joi);
-const mongoose     = require('mongoose');
-const helmet       = require('helmet');
-const morgan       = require('morgan');
-const debugstartup = require('debug')('vidly::startup');
-const debugdb      = require('debug')('vidly::db');
-const config       = require('config');
-const express      = require('express');
-const genres       = require('./routes/genres');
-const customers    = require('./routes/customers');
-const movies       = require('./routes/movies');
-const rentals      = require('./routes/rentals');
-const users        = require('./routes/users');
-const auth         = require('./routes/auth');
-const error        = require('./middleware/error');
-const winston      = require('winston');
-                     require('winston-mongodb');
-
-winston.exceptions.handle(
-    new winston.transports.File({filename: 'logs/uncaughtException.log'})
-);
-
-winston.rejections.handle(
-    new winston.transports.File({filename: 'logs/unhandledRejection.log'})
-);
-
-winston.add(new winston.transports.File({filename: 'logs/logfile.log'}));
-winston.add(new winston.transports.MongoDB({
-    db: 'mongodb://localhost/vidly',
-    level: 'error'
-}));
-
-debugstartup('App Name: ' + config.get('name'));
-
-mongoose.connect('mongodb://localhost/vidly')
-.then(() => debugdb("Connected to mongodb..."))
-.catch((err) => debugdb(err.message));
+const winston = require('winston');
+const express = require('express');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(helmet());
-
-if(app.get('env') === 'development') {
-    app.use(morgan('tiny'));
-    debugstartup('morgan is enabled...');
-}
-
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
 
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { debugstartup(`Listen to port ${port}`) });
+app.listen(port, () => { winston.info(`Listening on port ${port}...`) });
