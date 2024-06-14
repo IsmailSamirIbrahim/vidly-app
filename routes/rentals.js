10102825cc4b9424c49dc7d257e4ae4725b57c5e
Ismail Samir Ibrahim <ismail.samir.ibrahim@gmail.com>
2024-06-14 19:15:44 +0300

    Cleanup the code (#29)

--- routes/rentals.js
+++ routes/rentals.js
@@ -74,77 +74,11 @@ router.get('/:id', asyncMiddleware(async (req, res) => {
     res.send(rental);
 }));
 
-// update specific rental
-router.put('/:id', auth, asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if(error)
-        return res.status(400).send(error.details[0].message);
-    
-    const oldRental = await Rental.findById(req.params.id);
-    if(!oldRental)
-        return res.status(404).send(`A rental with the given id:${req.params.id} is not found.`);
-
-    if(oldRental.movie._id !== req.body.movieId) {
-        const oldMovie = await Movie.findByIdAndUpdate(oldRental.movie._id, {
-            $inc: {numberInStock: 1}
-        });
-        if(!oldMovie)
-            return res.status(404).send(`A movie with the given id:${oldRental.movie._id} is not found.`);
-    }
-    
-    const customer = await Customer.findById(req.body.customerId);
-    if(!customer)
-        return res.status(404).send(`A customer with the given id:${req.body.customerId} is not found.`);
-
-    const movie = await Movie.findById(req.body.movieId);
-    if(!movie)
-        return res.status(404).send(`A movie with the given id:${req.body.movieId} is not found.`);
-
-    if(movie.numberInStock === 0)
-        return res.status(400).send('Movie not in stock.');
-
-    movie.numberInStock -= 1;
-    movie.dailyRentalRate += 1;
-
-    let rental = await Rental.findById(req.params.id);
-    if(!rental)
-        return res.status(404).send(`A rental with the given id:${req.params.id} is not found.`);
-
-    rental.customer = {
-        _id: customer._id,
-        name: customer.name,
-        phone: customer.phone,
-        isGold: customer.isGold
-    };
-    rental.movie = {
-        _id: movie._id,
-        name: movie.name,
-        genre: {
-            _id: movie.genre._id,
-            name: movie.genre.name
-        },
-        numberInStock: movie.numberInStock,
-        dailyRentalRate: movie.dailyRentalRate
-    };
-    
-    // [TODO]: use two phase commit here...
-    await rental.save();
-    await movie.save();
-
-    res.send(rental);
-}));
-
 // delete specific rental
 router.delete('/:id', [auth, admin], asyncMiddleware(async (req, res) => {
     const rental = await Rental.findByIdAndDelete(req.params.id);
     if(!rental)
         return res.status(404).send(`A rental with the given id:${req.params.id} is not found.`);
-
-    const movie = await Movie.findByIdAndUpdate(rental.movie._id, {
-        $inc: {numberInStock: 1}
-    });
-    if(!movie)
-        return res.status(404).send(`A movie with the given id:${rental.movie._id} is not found.`);
 
     res.send(rental);
 }));
